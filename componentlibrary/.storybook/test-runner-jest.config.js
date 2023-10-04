252cd545dd49f6fa3858f6b7b531259f6e573939
Louis Jackson-Rees <louis.jacksonrees@bankbi.com>
2023-10-04 12:52:01 +0100

    remove jest config from testing

--- componentlibrary/.storybook/test-runner-jest.config.js
+++
@@ -1,7 +0,0 @@
-const { getJestConfig } = require('@storybook/test-runner');
-
-const resolver = require('@nx/jest/plugins/resolver').default;
-module.exports = {
-  ...getJestConfig(),
-  resolver: resolver,
-};
